dd660e2d4951e291b417eac6ca7d2fbfbb7c5ae9
eclendenning-va <evan.clendenning@libertyits.com>
2021-02-05 09:38:32 -0500

    precompile pattern

--- vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/controller/R4Transformers.java
+++ vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/controller/R4Transformers.java
@@ -1,17 +1,19 @@
 package gov.va.api.health.vistafhirquery.service.controller;
 
 import java.math.BigDecimal;
+import java.util.regex.Pattern;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class R4Transformers {
+  private static final Pattern BIG_DECIMAL_PATTERN = Pattern.compile("\\d+(\\.\\d+)?");
 
   /** Creates a BigDecimal from a string if possible, otherwise returns null. */
   public static BigDecimal toBigDecimal(String string) {
     if (string == null) {
       return null;
     }
-    if (string.matches("\\d+(\\.\\d+)?")) {
+    if (BIG_DECIMAL_PATTERN.matcher(string).matches()) {
       return new BigDecimal(string);
     }
     return null;
