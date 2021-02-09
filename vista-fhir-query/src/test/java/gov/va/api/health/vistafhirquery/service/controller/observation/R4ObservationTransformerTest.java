dd48eb7bd26540d329313fbe0cf74668676b4ac9
jhulbert <joshua.hulbert@libertyits.com>
2021-02-09 08:39:36 -0500

    Tests

--- vista-fhir-query/src/test/java/gov/va/api/health/vistafhirquery/service/controller/observation/R4ObservationTransformerTest.java
+++ vista-fhir-query/src/test/java/gov/va/api/health/vistafhirquery/service/controller/observation/R4ObservationTransformerTest.java
@@ -6,9 +6,19 @@ import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 public class R4ObservationTransformerTest {
+  @Test
+  void labToFhir() {
+    assertThat(
+            R4ObservationTransformer.builder()
+                .patientIcn("p1")
+                .resultsEntry(ObservationLabSamples.Vista.create().resultsByStation())
+                .build()
+                .toFhir())
+        .containsExactly(ObservationLabSamples.Fhir.create().observation());
+  }
 
   @Test
-  public void ToFhir() {
+  public void vitalToFhir() {
     assertThat(
             R4ObservationTransformer.builder()
                 .resultsEntry(ObservationSamples.Vista.create().resultsByStation())
