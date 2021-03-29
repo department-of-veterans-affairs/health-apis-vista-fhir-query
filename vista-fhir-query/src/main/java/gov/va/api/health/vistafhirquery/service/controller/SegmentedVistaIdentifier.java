5571460be9dbc02470ce86f3e7ca8ed48e15154a
Bryan Schofield <bryan.schofield@libertyits.com>
2021-03-29 18:56:57 -0400

    wip - binary encoder, but it sux:

--- vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/controller/SegmentedVistaIdentifier.java
+++ vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/controller/SegmentedVistaIdentifier.java
@@ -1,17 +1,27 @@
 package gov.va.api.health.vistafhirquery.service.controller;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
+import gov.va.api.health.ids.client.AsciiCompressor;
 import gov.va.api.lighthouse.charon.models.vprgetpatientdata.VprGetPatientData;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Optional;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 
 @Value
 @Builder
+@Slf4j
 public class SegmentedVistaIdentifier {
   @NonNull PatientIdentifierType patientIdentifierType;
 
@@ -30,6 +40,7 @@ public class SegmentedVistaIdentifier {
   }
 
   /** Parse a VistaIdentifier. */
+  @Deprecated
   public static SegmentedVistaIdentifier parse(String id) {
     String[] segmentParts = id.split("\\+", -1);
     if (segmentParts.length != 3) {
@@ -56,7 +67,18 @@ public class SegmentedVistaIdentifier {
         .build();
   }
 
+  /** Parse a VistaIdentifier. */
+  public static SegmentedVistaIdentifier unpack(String id) {
+    return new Encoder().unpack(id);
+  }
+
   /** Build a VistaIdentifier. */
+  public String pack() {
+    return new Encoder().pack(this);
+  }
+
+  /** Build a VistaIdentifier. */
+  @Deprecated
   public String toIdentifierSegment() {
     return String.join(
         "+",
@@ -89,6 +111,227 @@ public class SegmentedVistaIdentifier {
           throw new IllegalArgumentException(
               "PatientIdentifierType abbreviation in segment is invalid: " + abbreviation);
       }
+    }
+  }
+
+  private interface Format {
+
+    String tryPack(SegmentedVistaIdentifier vis);
+
+    SegmentedVistaIdentifier unpack(String data);
+  }
+
+  private static class BinaryFormat implements Format {
+
+    Optional<Integer> asInt(String maybeInt) {
+      try {
+        return Optional.of(Integer.parseInt(maybeInt));
+      } catch (NumberFormatException e) {
+        return Optional.empty();
+      }
+    }
+
+    @Override
+    public String tryPack(SegmentedVistaIdentifier vis) {
+      if (vis.patientIdentifierType() != PatientIdentifierType.NATIONAL_ICN) {
+        return null;
+      }
+      var tenSix = TenVSix.parse(vis.patientIdentifier());
+      if (tenSix.isEmpty()) {
+        return null;
+      }
+      var site = asInt(vis.vistaSiteId());
+      if (site.isEmpty()) {
+        return null;
+      }
+
+      var recordIdChars = vis.vistaRecordId().toCharArray();
+      ByteBuffer binary =
+          ByteBuffer.allocate(
+              /* 10 digits of icn */
+              Long.BYTES
+                  /* 6 digits of icn */
+                  + Integer.BYTES
+                  /* Site ID */
+                  + Short.BYTES
+                  /* vpr rpc domain ordinal */
+                  + Short.BYTES
+                  /* Number of chars in record ID */
+                  + Short.BYTES
+                  /* The record ID */
+                  + (recordIdChars.length * Character.BYTES));
+      binary.putLong(tenSix.get().ten());
+      binary.putInt(tenSix.get().six());
+      binary.putShort(site.get().shortValue());
+      binary.putShort((short) vis.vprRpcDomain().ordinal());
+      binary.putShort((short) recordIdChars.length);
+      for (char c : recordIdChars) {
+        binary.putChar(c);
+      }
+      log.info("at {} of {}", binary.position(), binary.limit());
+      log.info("ARRAY {}", Arrays.toString(binary.array()));
+      return new String(binary.array(), StandardCharsets.ISO_8859_1);
+    }
+
+    @Override
+    public SegmentedVistaIdentifier unpack(String data) {
+      ByteBuffer binary = ByteBuffer.wrap(data.getBytes(StandardCharsets.ISO_8859_1));
+      long ten = binary.getLong();
+      log.info("ten {}", ten);
+      int six = binary.getInt();
+      log.info("siz {}", six);
+      int site = binary.getShort();
+      log.info("site {}", site);
+      int domainOrdinal = binary.getShort();
+      log.info("domain {}", domainOrdinal);
+      int recordIdCharsSize = binary.getShort();
+      log.info("chars {}", recordIdCharsSize);
+      StringBuilder recordId = new StringBuilder(recordIdCharsSize);
+      for (int i = 0; i < recordIdCharsSize; i++) {
+        recordId.append(binary.getChar());
+      }
+      return SegmentedVistaIdentifier.builder()
+          .patientIdentifierType(PatientIdentifierType.NATIONAL_ICN)
+          .patientIdentifier(TenVSix.builder().ten(ten).six(six).build().asIcn())
+          .vistaSiteId(Integer.toString(site))
+          .vprRpcDomain(VprGetPatientData.Domains.values()[domainOrdinal])
+          .vistaRecordId(recordId.toString())
+          .build();
+    }
+  }
+
+  private static class CompressedAsciiFormat implements Format {
+
+    private final AsciiCompressor compressor = new AsciiCompressor();
+    private Format delegate = new StringFormat();
+
+    @Override
+    public String tryPack(SegmentedVistaIdentifier vis) {
+      return new String(compressor.compress(delegate.tryPack(vis)), StandardCharsets.ISO_8859_1);
+    }
+
+    @Override
+    public SegmentedVistaIdentifier unpack(String data) {
+      log.error("I AM GARBAGE. I EAT YOUR DATAS. LOL.");
+      log.info("Unpacking compressed ID {}", data);
+      String decompressed = compressor.decompress(data.getBytes(StandardCharsets.ISO_8859_1));
+      log.info("ID {}", decompressed);
+      return delegate.unpack(decompressed);
+    }
+  }
+
+  private static class Encoder {
+    private final Map<Character, Format> formats;
+
+    Encoder() {
+      formats = new LinkedHashMap<>();
+      // formats.put('c', new CompressedAsciiFormat());
+      formats.put('b', new BinaryFormat());
+      /* StringFormat is the failsafe format, this should be last. */
+      formats.put('s', new StringFormat());
+    }
+
+    /** Build a VistaIdentifier. */
+    public String pack(SegmentedVistaIdentifier vis) {
+      for (var entry : formats.entrySet()) {
+        String value = entry.getValue().tryPack(vis);
+        if (value != null) {
+          log.info(
+              "PACK: ({}) {} with {} as ({}) {}",
+              vis.toIdentifierSegment().getBytes(StandardCharsets.UTF_8).length,
+              vis.toIdentifierSegment(),
+              entry.getKey(),
+              value.length(),
+              value);
+          return entry.getKey() + value;
+        }
+      }
+      throw new IllegalStateException(
+          "VistaIdentifierSegment should be been encoded by StringFormat");
+    }
+
+    public SegmentedVistaIdentifier unpack(String data) {
+      if (isBlank(data)) {
+        throw new IllegalArgumentException("blank identifier");
+      }
+      log.info("DATA {} {}", data, data.length());
+      char formatId = data.charAt(0);
+      Format format = formats.get(formatId);
+      /* Support old format IDs that have no format prefix, but are still string formatted */
+      if (format == null) {
+        return formats.get('s').unpack(data);
+      }
+      log.info("UNPACK: {} {}", formatId, data);
+      return format.unpack(data.substring(1));
+    }
+  }
+
+  private static class StringFormat implements Format {
+    @Override
+    public String tryPack(SegmentedVistaIdentifier vis) {
+      return vis.toIdentifierSegment();
+    }
+
+    @Override
+    public SegmentedVistaIdentifier unpack(String data) {
+      String[] segmentParts = data.split("\\+", -1);
+      if (segmentParts.length != 3) {
+        throw new IllegalArgumentException(
+            "SegmentedVistaIdentifier are expected to have 3 parts "
+                + "(e.g. patientIdTypeAndId+vistaSiteId+vistaRecordId).");
+      }
+      if (segmentParts[0].length() < 2 || segmentParts[2].length() < 2) {
+        throw new IllegalArgumentException(
+            "The first and third sections of a SegmentedVistaIdentifier must contain "
+                + "a type and an identifier value.");
+      }
+      var domainType = domainAbbreviationMappings().get(segmentParts[2].charAt(0));
+      if (domainType == null) {
+        throw new IllegalArgumentException(
+            "Identifier value had invalid domain type abbreviation: " + segmentParts[2].charAt(0));
+      }
+      log.info("Parts {}", Arrays.toString(segmentParts));
+      return SegmentedVistaIdentifier.builder()
+          .patientIdentifierType(PatientIdentifierType.fromAbbreviation(segmentParts[0].charAt(0)))
+          .patientIdentifier(segmentParts[0].substring(1))
+          .vistaSiteId(segmentParts[1])
+          .vprRpcDomain(domainType)
+          .vistaRecordId(segmentParts[2].substring(1))
+          .build();
+    }
+  }
+
+  @Value
+  @Builder
+  private static class TenVSix {
+    long ten;
+    int six;
+
+    static Optional<TenVSix> parse(String icn) {
+      if (isBlank(icn)) {
+        return Optional.empty();
+      }
+      try {
+        /* Attempt to find national ICN in 10V6 format. */
+        if (icn.length() == 10 + 1 + 6 && icn.charAt(10) == 'V') {
+          return Optional.of(
+              TenVSix.builder()
+                  .ten(Long.parseLong(icn.substring(0, 10)))
+                  .six(Integer.parseInt(icn.substring(11)))
+                  .build());
+        }
+        /* Attempt to find all numeric lab-style ID. */
+        return Optional.of(TenVSix.builder().ten(Long.parseLong(icn)).six(0).build());
+      } catch (NumberFormatException e) {
+        return Optional.empty();
+      }
+    }
+
+    String asIcn() {
+      if (six == 0) {
+        return "" + ten;
+      }
+      return ten + "V" + six;
     }
   }
 }
--- vista-fhir-query/src/test/java/gov/va/api/health/vistafhirquery/service/controller/SegmentedVistaIdentifierTest.java
+++ vista-fhir-query/src/test/java/gov/va/api/health/vistafhirquery/service/controller/SegmentedVistaIdentifierTest.java
@@ -3,13 +3,24 @@ package gov.va.api.health.vistafhirquery.service.controller;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
+import gov.va.api.health.ids.api.ResourceIdentity;
+import gov.va.api.health.ids.client.IdsClientProperties;
+import gov.va.api.health.ids.client.IdsClientProperties.EncodedIdsFormatProperties;
+import gov.va.api.health.ids.client.RestIdentityServiceClientConfig;
+import gov.va.api.health.vistafhirquery.idsmapping.VistaFhirQueryIdsCodebookSupplier;
+import gov.va.api.health.vistafhirquery.service.controller.SegmentedVistaIdentifier.PatientIdentifierType;
 import gov.va.api.lighthouse.charon.models.vprgetpatientdata.VprGetPatientData;
+import gov.va.api.lighthouse.charon.models.vprgetpatientdata.VprGetPatientData.Domains;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+@Slf4j
 public class SegmentedVistaIdentifierTest {
+  @SuppressWarnings("ResultOfMethodCallIgnored")
   @Test
   void invalidPatientIdentifierTypeThrowsIllegalArgument() {
     assertThatExceptionOfType(IllegalArgumentException.class)
@@ -17,8 +28,67 @@ public class SegmentedVistaIdentifierTest {
   }
 
   @Test
+  void packWithBinaryFormat() {
+    // VISTA Observation N1011537977V693883+673+LCH;6929384.839997;14
+    SegmentedVistaIdentifier id =
+        SegmentedVistaIdentifier.builder()
+            .patientIdentifierType(PatientIdentifierType.NATIONAL_ICN)
+            .patientIdentifier("1011537977V693883")
+            .vistaSiteId("673")
+            .vprRpcDomain(Domains.labs)
+            .vistaRecordId("CH;6929384.839997;14xxx")
+            .build();
+    String packed = id.pack();
+    SegmentedVistaIdentifier unpacked = SegmentedVistaIdentifier.unpack(packed);
+    assertThat(unpacked).isEqualTo(id);
+    var ids =
+        new RestIdentityServiceClientConfig(
+                null,
+                IdsClientProperties.builder()
+                    .encodedIds(
+                        EncodedIdsFormatProperties.builder()
+                            .i3Enabled(true)
+                            .encodingKey("some-longish-key-here")
+                            .build())
+                    .build(),
+                null)
+            .encodingIdentityServiceClient(new VistaFhirQueryIdsCodebookSupplier().get());
+    String i3 =
+        ids.register(
+                List.of(
+                    ResourceIdentity.builder()
+                        .system("VISTA")
+                        .resource("Observation")
+                        .identifier(packed)
+                        .build()))
+            .get(0)
+            .uuid();
+    assertThat(i3.length()).as(packed).isLessThanOrEqualTo(64);
+    log.info(
+        "({}) {} -> ({}) {}",
+        id.toIdentifierSegment().length(),
+        id.toIdentifierSegment(),
+        i3.length(),
+        i3);
+  }
+
+  @Test
+  void packWithStringFormat() {
+    assertThat(
+            SegmentedVistaIdentifier.builder()
+                .patientIdentifierType(SegmentedVistaIdentifier.PatientIdentifierType.NATIONAL_ICN)
+                .patientIdentifier("icn")
+                .vistaSiteId("siteId")
+                .vprRpcDomain(VprGetPatientData.Domains.vitals)
+                .vistaRecordId("vistaId")
+                .build()
+                .pack())
+        .isEqualTo("sNicn+siteId+VvistaId");
+  }
+
+  @Test
   void parseIdSuccessfully() {
-    assertThat(SegmentedVistaIdentifier.parse("Nicn+siteId+LvistaId"))
+    assertThat(SegmentedVistaIdentifier.unpack("Nicn+siteId+LvistaId"))
         .isEqualTo(
             SegmentedVistaIdentifier.builder()
                 .patientIdentifierType(SegmentedVistaIdentifier.PatientIdentifierType.NATIONAL_ICN)
@@ -34,7 +104,7 @@ public class SegmentedVistaIdentifierTest {
       strings = {"x+123+Vabc", "+123+abc", "123", "123+abc", "D123+abc+V456+def", "D123+abc+x"})
   void parseInvalidSegmentThrowsIllegalArgument(String segment) {
     assertThatExceptionOfType(IllegalArgumentException.class)
-        .isThrownBy(() -> SegmentedVistaIdentifier.parse(segment));
+        .isThrownBy(() -> SegmentedVistaIdentifier.unpack(segment));
   }
 
   @ParameterizedTest
