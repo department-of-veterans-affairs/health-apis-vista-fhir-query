1f419b845fc85a28729dd3d47de87e4125ea1d81
ilaflamme <ian.laflamme@libertyits.com>
2021-04-27 14:30:02 -0400

    multiple principal config options

--- vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/config/VistaApiConfig.java
+++ vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/config/VistaApiConfig.java
@@ -2,6 +2,17 @@ package gov.va.api.health.vistafhirquery.service.config;
 
 import static org.apache.commons.lang3.StringUtils.trimToNull;
 
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gov.va.api.lighthouse.charon.api.RpcPrincipal;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -32,7 +43,57 @@ public class VistaApiConfig {
 
   private String applicationProxyUserContext;
 
+  private String siteSpecificPrincipalFile;
+
+  private Map<String, SiteSpecificDetails> siteSpecificDetails;
+
   public String getApplicationProxyUserContext() {
     return trimToNull(applicationProxyUserContext);
+  }
+
+  /** The value "default" is validated during @PostConstruct, and will be present. */
+  public RpcPrincipal getPrincipalOrDefault(String site) {
+    return getSiteSpecificDetails()
+        .getOrDefault(site, getSiteSpecificDetails().get("default"))
+        .getRpcPrincipal();
+  }
+
+  @PostConstruct
+  void loadSiteSpecificInfoFromFile() {
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNode root;
+    try (FileInputStream fs = new FileInputStream(siteSpecificPrincipalFile)) {
+      root = mapper.readTree(fs);
+    } catch (FileNotFoundException e) {
+      throw new IllegalArgumentException(
+          "Failed to find principals file: " + siteSpecificPrincipalFile);
+    } catch (IOException e) {
+      throw new IllegalArgumentException("Failed to parse json file: " + siteSpecificPrincipalFile);
+    }
+    if (root.isEmpty()) {
+      throw new IllegalArgumentException("Principal file is empty: " + siteSpecificPrincipalFile);
+    }
+    Map<String, SiteSpecificDetails> siteToDetailsMap = new HashMap<>();
+    JsonNode siteSpecificPrincipalsNode = root.get("siteSpecificPrincipals");
+    if (!siteSpecificPrincipalsNode.isNull()) {
+      siteToDetailsMap = mapper.convertValue(siteSpecificPrincipalsNode, new TypeReference<>() {});
+    }
+    JsonNode defaultPrincipalNode = root.get("principal");
+    if (defaultPrincipalNode.isNull()) {
+      throw new IllegalArgumentException(
+          "Default rpc principal is not defined in: " + siteSpecificPrincipalFile);
+    }
+    RpcPrincipal defaultPrincipal = mapper.convertValue(root.get("principal"), RpcPrincipal.class);
+    siteToDetailsMap.put(
+        "default", SiteSpecificDetails.builder().rpcPrincipal(defaultPrincipal).build());
+    siteSpecificDetails = Map.copyOf(siteToDetailsMap);
+  }
+
+  @Data
+  @Builder
+  @NoArgsConstructor
+  @AllArgsConstructor
+  static class SiteSpecificDetails {
+    @JsonUnwrapped RpcPrincipal rpcPrincipal;
   }
 }
---
+++ vista-fhir-query/src/test/java/gov/va/api/health/vistafhirquery/service/config/VistaApiConfigTest.java
@@ -0,0 +1,29 @@
+package gov.va.api.health.vistafhirquery.service.config;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import gov.va.api.lighthouse.charon.api.RpcPrincipal;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class VistaApiConfigTest {
+
+  @Test
+  void loadSiteSpecificInfoFromFile() {
+    var config =
+        VistaApiConfig.builder()
+            .siteSpecificPrincipalFile("src/test/resources/site-specific-details.json")
+            .build();
+    config.loadSiteSpecificInfoFromFile();
+    assertThat(config.getPrincipalOrDefault("999"))
+        .isEqualTo(
+            RpcPrincipal.builder()
+                .applicationProxyUser("FAKE:APU@999")
+                .accessCode("FAKE:AC@999")
+                .verifyCode("FAKE:VC@999")
+                .build());
+    assertThat(config.getPrincipalOrDefault("123"))
+        .isEqualTo(RpcPrincipal.builder().accessCode("FAKE:AC").verifyCode("FAKE:VC").build());
+  }
+}
