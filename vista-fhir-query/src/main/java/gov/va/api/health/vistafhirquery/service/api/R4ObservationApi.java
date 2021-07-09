45c3f31ae4d36f6a9b4cfa051a969865d49f80a7
eclendenning-va <evan.clendenning@libertyits.com>
2021-07-09 16:17:43 -0400

    add observation 401,403,500 responses and examples for the parameters

--- vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/api/R4ObservationApi.java
+++ vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/api/R4ObservationApi.java
@@ -39,8 +39,32 @@ public interface R4ObservationApi {
               schema = @Schema(implementation = OperationOutcome.class))
         }),
     @ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized",
+        content = {
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = OperationOutcome.class))
+        }),
+    @ApiResponse(
+        responseCode = "403",
+        description = "Forbidden",
+        content = {
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = OperationOutcome.class))
+        }),
+    @ApiResponse(
         responseCode = "404",
         description = "Not found",
+        content = {
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = OperationOutcome.class))
+        }),
+    @ApiResponse(
+        responseCode = "500",
+        description = "Server Error",
         content = {
           @Content(
               mediaType = "application/fhir+json",
@@ -53,7 +77,8 @@ public interface R4ObservationApi {
               name = "id",
               required = true,
               description =
-                  "The logical id of the resource. Once assigned, this value never changes.")
+                  "The logical id of the resource. Once assigned, this value never changes.",
+              example = "I3-87aih87KJHaMAuy89PKA90AOIJL897QHJKNaPZha9jLJKHAO8hkja")
           String id);
 
   @Operation(
@@ -80,8 +105,32 @@ public interface R4ObservationApi {
               schema = @Schema(implementation = OperationOutcome.class))
         }),
     @ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized",
+        content = {
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = OperationOutcome.class))
+        }),
+    @ApiResponse(
+        responseCode = "403",
+        description = "Forbidden",
+        content = {
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = OperationOutcome.class))
+        }),
+    @ApiResponse(
         responseCode = "404",
         description = "Not found",
+        content = {
+          @Content(
+              mediaType = "application/fhir+json",
+              schema = @Schema(implementation = OperationOutcome.class))
+        }),
+    @ApiResponse(
+        responseCode = "500",
+        description = "Server Error",
         content = {
           @Content(
               mediaType = "application/fhir+json",
@@ -94,48 +143,55 @@ public interface R4ObservationApi {
               name = "patient",
               description =
                   "The patient's Integration Control Number (ICN)"
-                      + " assigned by the Master Patient Index (MPI).")
+                      + " assigned by the Master Patient Index (MPI).",
+              example = "1010101010V66666")
           String patient,
       @Parameter(
               in = ParameterIn.QUERY,
               name = "_id",
               description =
-                  "The logical id of the resource. Once assigned, this value never changes.")
+                  "The logical id of the resource. Once assigned, this value never changes.",
+              example = "I3-87aih87KJHaMAuy89PKA90AOIJL897QHJKNaPZha9jLJKHAO8hkja")
           String id,
       @Parameter(
               in = ParameterIn.QUERY,
               name = "identifier",
               description =
                   "The logical identifier of the resource. "
-                      + "Once assigned, this value never changes.")
+                      + "Once assigned, this value never changes.",
+              example = "I3-87aih87KJHaMAuy89PKA90AOIJL897QHJKNaPZha9jLJKHAO8hkja")
           String identifier,
       @Parameter(
               in = ParameterIn.QUERY,
               name = "category",
               description =
                   "The general classification of the type of observation. "
-                      + "[Observation Category Codes](https://www.hl7.org/fhir/valueset-observation-category.html)")
+                      + "[Observation Category Codes](https://www.hl7.org/fhir/valueset-observation-category.html)",
+              example = "laboratory")
           String category,
       @Parameter(
               in = ParameterIn.QUERY,
               name = "code",
               description =
                   "A code that indicates the type of information contained within the observation. "
-                      + "[LOINC Observation Codes](https://www.hl7.org/fhir/valueset-observation-codes.html)")
+                      + "[LOINC Observation Codes](https://www.hl7.org/fhir/valueset-observation-codes.html)",
+              example = "8310-5")
           String code,
       @Parameter(
               in = ParameterIn.QUERY,
               name = "date",
               description =
                   "A date or range of dates (maximum of 2) that describes the date that "
-                      + "the observation was recorded.")
+                      + "the observation was recorded.",
+              example = "ge2018-03-14T00:00:00Z")
           String[] date,
       @Parameter(
               in = ParameterIn.QUERY,
               name = "_count",
               description =
                   "The number of resources that should be returned in a single page. "
-                      + "The maximum count size is 100.")
+                      + "The maximum count size is 100.",
+              example = "30")
           @DefaultValue("30")
           int count);
 }
