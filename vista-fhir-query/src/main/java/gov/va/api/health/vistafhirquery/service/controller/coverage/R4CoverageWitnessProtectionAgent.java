e750c4f573e0cbf39fbd477eac22ebd176c15ec0
jabando-va <jesse.abando@libertyits.com>
2021-08-18 17:18:54 -0400

    Simplified check for only the index of id

--- vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/controller/coverage/R4CoverageWitnessProtectionAgent.java
+++ vista-fhir-query/src/main/java/gov/va/api/health/vistafhirquery/service/controller/coverage/R4CoverageWitnessProtectionAgent.java
@@ -4,8 +4,6 @@ import gov.va.api.health.r4.api.resources.Coverage;
 import gov.va.api.health.vistafhirquery.service.controller.witnessprotection.ProtectedReference;
 import gov.va.api.health.vistafhirquery.service.controller.witnessprotection.ProtectedReferenceFactory;
 import gov.va.api.health.vistafhirquery.service.controller.witnessprotection.WitnessProtectionAgent;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
 import java.util.stream.Stream;
 import lombok.NonNull;
@@ -19,28 +17,6 @@ import org.springframework.stereotype.Component;
 public class R4CoverageWitnessProtectionAgent implements WitnessProtectionAgent<Coverage> {
   private final ProtectedReferenceFactory protectedReferenceFactory;
 
-  /** Get id from a string, removing the prefix. */
-  public String getId(String id) {
-    if (!id.contains("/")) {
-      return id;
-    }
-    List<String> strings = Arrays.asList(id.split("/"));
-    return strings.get(strings.size() - 1);
-  }
-
-  /** Get prefix of a string, if it has one. */
-  public String getPrefix(String id) {
-    if (!id.contains("/")) {
-      return "";
-    }
-    List<String> strings = Arrays.asList(id.split("/"));
-    StringBuilder prefix = new StringBuilder("/");
-    for (int i = 0; i < strings.size() - 2; i++) {
-      prefix.append(strings.get(i)).append("/");
-    }
-    return prefix.toString();
-  }
-
   @Override
   public Stream<ProtectedReference> referencesOf(Coverage resource) {
     Stream<ProtectedReference> referenceGroups =
@@ -50,12 +26,14 @@ public class R4CoverageWitnessProtectionAgent implements WitnessProtectionAgent<
                 .filter(Objects::nonNull),
             resource.coverageClass().stream()
                 .map(
-                    c ->
-                        ProtectedReference.builder()
-                            .type("InsurancePlan")
-                            .id(getId(c.value()))
-                            .onUpdate(i -> c.value(getPrefix(c.value()) + i))
-                            .build()));
+                    c -> {
+                      int indexOfId = c.value().lastIndexOf('/') + 1;
+                      return ProtectedReference.builder()
+                          .type("InsurancePlan")
+                          .id(c.value().substring(indexOfId))
+                          .onUpdate(i -> c.value(c.value().substring(0, indexOfId) + i))
+                          .build();
+                    }));
     return Stream.concat(
         Stream.of(
             protectedReferenceFactory.forResource(resource, resource::id),
