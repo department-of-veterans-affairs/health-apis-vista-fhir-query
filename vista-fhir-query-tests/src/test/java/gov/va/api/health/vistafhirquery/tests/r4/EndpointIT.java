d667c4eb616a1ab581b5ca117700b0b5d883cfa6
jabando-va <jesse.abando@libertyits.com>
2021-08-26 15:39:08 -0400

    Using r4 testclient

--- vista-fhir-query-tests/src/test/java/gov/va/api/health/vistafhirquery/tests/r4/EndpointIT.java
+++ vista-fhir-query-tests/src/test/java/gov/va/api/health/vistafhirquery/tests/r4/EndpointIT.java
@@ -1,15 +1,10 @@
 package gov.va.api.health.vistafhirquery.tests.r4;
 
-import static gov.va.api.health.sentinel.EnvironmentAssumptions.assumeEnvironmentNotIn;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import gov.va.api.health.r4.api.resources.Endpoint;
-import gov.va.api.health.sentinel.Environment;
-import gov.va.api.health.sentinel.ExpectedResponse;
 import gov.va.api.health.vistafhirquery.tests.SystemDefinitions;
-import io.restassured.RestAssured;
-import io.restassured.http.Method;
-import io.restassured.specification.RequestSpecification;
+import gov.va.api.health.vistafhirquery.tests.TestClients;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -17,36 +12,24 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 @Slf4j
 public class EndpointIT {
-  private RequestSpecification request(gov.va.api.health.sentinel.ServiceDefinition sd) {
-    RequestSpecification request =
-        RestAssured.given()
-            .baseUri(sd.url())
-            .port(sd.port())
-            .relaxedHTTPSValidation()
-            .contentType("application/json")
-            .accept("application/json");
-    return request;
-  }
+
+  private final String urlWithPath = SystemDefinitions.systemDefinition().r4().urlWithApiPath();
 
   @ParameterizedTest
   @ValueSource(strings = {"Endpoint", "Endpoint?status=active"})
   void search(String query) {
-    assumeEnvironmentNotIn(Environment.STAGING, Environment.PROD);
-    var sd = SystemDefinitions.systemDefinition().r4();
-    ExpectedResponse bundleResponse =
-        ExpectedResponse.of(request(sd).request(Method.GET, sd.apiPath() + query));
-    var bundle = bundleResponse.expect(200).expectValid(Endpoint.Bundle.class);
+    var bundle =
+        TestClients.r4().get(urlWithPath + query).expect(200).expectValid(Endpoint.Bundle.class);
     assertThat(bundle.total()).isGreaterThan(0);
   }
 
   @Test
   void searchWithBadStatus() {
-    assumeEnvironmentNotIn(Environment.STAGING, Environment.PROD);
-    var sd = SystemDefinitions.systemDefinition().r4();
-    ExpectedResponse bundleResponse =
-        ExpectedResponse.of(
-            request(sd).request(Method.GET, sd.apiPath() + "Endpoint?status=INVALID"));
-    var bundle = bundleResponse.expect(200).expectValid(Endpoint.Bundle.class);
+    var bundle =
+        TestClients.r4()
+            .get(urlWithPath + "Endpoint?status=INVALID")
+            .expect(200)
+            .expectValid(Endpoint.Bundle.class);
     assertThat(bundle.total()).isEqualTo(0);
   }
 }
